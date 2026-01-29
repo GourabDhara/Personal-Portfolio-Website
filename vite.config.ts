212a37f5dfa9d4dff89dbb7acc2eba65efb06036
GourabDhara <160488308+GourabDhara@users.noreply.github.com>
2026-01-29 16:42:51 +0530

    Fix router for Vercel (remove basename)

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  // ✅ REQUIRED for GitHub Pages
-  base: "/Personal-Portfolio-Website/",
-
-  plugins: [
-    react(), // ✅ keep only react for production
-  ],
-
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
